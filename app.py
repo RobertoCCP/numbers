90674c7338500a52f0e189a2db8969a3ddcf8695
Calculadora_RobertoCuascota <rccuascotap@utn.edu.ec>
2024-04-28 18:28:21 -0500

    as

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 from flask import Flask, render_template, request, jsonify
 import numpy as np
 from keras.models import load_model
-from keras.optimizers import Adam  
-from PIL import Image
+import imageio
 
 app = Flask(__name__)
 
-# Cargar el modelo entrenado y compilarlo
-model = load_model('mnist_cnn_model.h5')
-model.compile(loss='categorical_crossentropy', optimizer=Adam(), metrics=['accuracy'])
+# Cargar el modelo entrenado
+model = load_model('mnist_cnn_model.h5')  # Asegúrate de que el modelo esté en el mismo directorio que este archivo
 
 @app.route('/')
 def index():
@@ -16,21 +14,25 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    try:
-        # Leer la imagen y convertirla a escala de grises
-        img = Image.open(request.files['image']).convert('L').resize((28, 28))
-        
-        # Preparar la imagen para la predicción
-        img_array = np.array(img).reshape(1, 28, 28, 1).astype('float32') / 255
-        
-        # Realizar la predicción
-        prediction = model.predict(img_array)
-        predicted_number = np.argmax(prediction)
-        
-        # Devolver el resultado como JSON
-        return jsonify({'predicted_number': str(predicted_number)})
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+    # Obtener la imagen enviada desde el frontend
+    file = request.files['image']
+    im = imageio.imread(file)  # Leer la imagen
+
+    # Preprocesar la imagen
+    gray = np.dot(im[..., :3], [0.299, 0.587, 0.114])
+    if gray.shape != (28, 28):
+        gray = gray[:28, :28]
+    
+    # Preparar la imagen para la predicción
+    gray = gray.reshape(1, 28, 28, 1)
+    gray = gray.astype('float32') / 255
+    
+    # Realizar la predicción
+    prediction = model.predict(gray)
+    predicted_number = prediction.argmax()
+
+    # Devolver el resultado como JSON
+    return jsonify({'predicted_number': str(predicted_number)})
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
