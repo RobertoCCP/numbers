2f7b245e13424309086f70972265b13b2d18bb42
Calculadora_RobertoCuascota <rccuascotap@utn.edu.ec>
2024-04-28 18:41:14 -0500

    i

--- edit_requirements.py
+++ edit_requirements.py
@@ -16,5 +16,21 @@ with open("requirements.txt", "w") as f:
                 and "termcolor" not in line \
                 and "typing-extensions" not in line \
                 and "werkzeug" not in line \
-                and "mdurl" not in line:
+                and "mdurl" not in line \
+                and "blinker" not in line \
+                and "absl-py" not in line \
+                and "google-pasta" not in line \
+                and "grpcio" not in line \
+                and "itsdangerous" not in line \
+                and "click" not in line \
+                and "h5py" not in line \
+                and "six" not in line \
+                and "astunparse" not in line \
+                and "flatbuffers" not in line \
+                and "tensorboard-data-server" not in line \
+                and "pygments" not in line \
+                and "certifi" not in line \
+                and "charset-normalizer" not in line \
+                and "idna" not in line \
+                and "urllib3" not in line:
             f.write(line)
