4a26e6d67d25ba76499feba97f4047b782b9c2be
Calculadora_RobertoCuascota <rccuascotap@utn.edu.ec>
2024-04-28 15:54:56 -0500

    s

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
 from flask import Flask, render_template, request, jsonify
 import numpy as np
 from keras.models import load_model
-from keras.optimizers import Adam  # Importa el optimizador que usaste durante el entrenamiento
+from keras.optimizers import Adam  
 from PIL import Image
 
 app = Flask(__name__)
 
-# Cargar el modelo entrenado
-model = load_model('mnist_cnn_model.h5')  # Asegúrate de que el modelo esté en el mismo directorio que este archivo
-
-# Compila el modelo después de cargarlo
-optimizer = Adam()  # Configura el optimizador
-model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+# Cargar el modelo entrenado y compilarlo
+model = load_model('mnist_cnn_model.h5')
+model.compile(loss='categorical_crossentropy', optimizer=Adam(), metrics=['accuracy'])
 
 @app.route('/')
 def index():
@@ -19,21 +16,15 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Obtener la imagen enviada desde el frontend
-    file = request.files['image']
-    
     try:
         # Leer la imagen y convertirla a escala de grises
-        im = Image.open(file).convert('L')
-        im = im.resize((28, 28))  # Asegúrate de que la imagen tenga las dimensiones correctas
-        im = np.array(im)
+        img = Image.open(request.files['image']).convert('L').resize((28, 28))
         
         # Preparar la imagen para la predicción
-        gray = im.reshape(1, 28, 28, 1)
-        gray = gray.astype('float32') / 255
+        img_array = np.array(img).reshape(1, 28, 28, 1).astype('float32') / 255
         
         # Realizar la predicción
-        prediction = model.predict(gray)
+        prediction = model.predict(img_array)
         predicted_number = np.argmax(prediction)
         
         # Devolver el resultado como JSON
