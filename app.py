d804cede393d9b1f956c52b9809abfe50133ab1e
Calculadora_RobertoCuascota <rccuascotap@utn.edu.ec>
2024-04-28 15:31:47 -0500

    ssa

--- app.py
+++ app.py
@@ -1,47 +1,40 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
 import numpy as np
-import tensorflow as tf
 from keras.models import load_model
-from keras.preprocessing.image import img_to_array
 from PIL import Image
 
 app = Flask(__name__)
 
-# Cargar el modelo Keras
-model = load_model('model.h5')
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+# Cargar el modelo entrenado
+model = load_model('mnist_cnn_model.h5')  # Asegúrate de que el modelo esté en el mismo directorio que este archivo
 
 @app.route('/')
-def home():
-    return "Servicio activo"
+def index():
+    return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Verificar que se haya enviado un archivo
-    if 'file' not in request.files:
-        return jsonify({'error': 'No se encontró ningún archivo en la solicitud'}), 400
-    
-    # Leer el archivo de imagen
-    file = request.files['file']
-    if file.filename == '':
-        return jsonify({'error': 'Nombre de archivo vacío'}), 400
+    # Obtener la imagen enviada desde el frontend
+    file = request.files['image']
     
     try:
-        # Abrir la imagen
-        im = Image.open(file.stream)
+        # Leer la imagen y convertirla a escala de grises
+        im = Image.open(file).convert('L')
+        im = im.resize((28, 28))  # Asegúrate de que la imagen tenga las dimensiones correctas
+        im = np.array(im)
         
-        # Preprocesar la imagen
-        im = im.resize((128, 128))
-        im = img_to_array(im)
-        im = np.expand_dims(im, axis=0)
+        # Preparar la imagen para la predicción
+        gray = im.reshape(1, 28, 28, 1)
+        gray = gray.astype('float32') / 255
         
         # Realizar la predicción
-        prediction = model.predict(im)
-        predicted_class = np.argmax(prediction)
+        prediction = model.predict(gray)
+        predicted_number = np.argmax(prediction)
         
-        return jsonify({'predicted_class': int(predicted_class)}), 200
+        # Devolver el resultado como JSON
+        return jsonify({'predicted_number': str(predicted_number)})
     except Exception as e:
-        return jsonify({'error': f'Error al procesar la imagen: {str(e)}'}), 500
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
